7b1c963f10940adea93b68ecd0142e077fd0f4f6
karlhartfiel <116031482+karlhartfiel@users.noreply.github.com>
2022-10-17 20:20:32 +0100

    Update swap-first-and-last.py

--- swap-first-and-last.py
+++ swap-first-and-last.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 s = input()
-length = len(s)
-output = s[length - 1] + s[1:length - 1] + s[0]
 
-print(output)
+length = len(s)
+
+print(s[length - 1] + s[1:length - 1] + s[0])
+
